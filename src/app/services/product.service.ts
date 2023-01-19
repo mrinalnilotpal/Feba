78287e2b227f57d9cbf2dab20df3364cc78bce26
Nilotpal Mrinal <nilotpal.avanti@gmail.com>
2023-01-19 14:57:50 +0530

    first commit

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http: HttpClient) { }
+  addProduct(data:object){
+   return this.http.post('http://localhost:3000/products',data);
+  }
+
+  productList(){
+    return this.http.get<any[]>('http://localhost:3000/products')
+  }
+
+}
